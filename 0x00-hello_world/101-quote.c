7ce068186fecd3b3f82e6b9d5f3d9e07cd487943
malakshahin <mala2004eslam@gmail.com>
2023-03-16 16:41:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
+
  /**
   * main - Entry point 
   * Return: Always 1
  */
 
-int main() {
+int main() 
+{
     char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, message, 38);
     return 1;

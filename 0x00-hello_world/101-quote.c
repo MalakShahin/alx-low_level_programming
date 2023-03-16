586e0d7644e5cac95c152f047678ea1245cd040b
malakshahin <mala2004eslam@gmail.com>
2023-03-16 16:39:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
+ /**
+  * main - Entry point 
+  * Return: Always 1
+ */
 
-/**
- * main - Entry point 
- * Return : Always 1 
-*/
-
-int main(void)
-{
-	write(2, "and that piece of art is usefil\n" - Dora Korpar, "2015-10-19\n" , 59);
-	return (1);
+int main() {
+    char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, 38);
+    return 1;
 }	

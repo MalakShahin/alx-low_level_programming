ecffe0f44e484a717da280aa1dcb59e9e8e19f57
malakshahin <mala2004eslam@gmail.com>
2023-03-17 06:30:17 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@
      {
      printf("Last digit of %d is %d and is 0", n, ld);
      }
-     else 
+     else
      {
      printf("Last digit of %d is %d and is less than 6 and not 0", n, ld);
      }
